de310592eeb9f6185ac002071dba9d0a4303ee63
Skyler Clark <sclark@freolic.com>
2021-06-16 08:15:32 -0400

    Add FilterTags and GetTagsWithParent blueprint functions

--- Source/BetterGameplayTags/Private/BGTBlueprintLibrary.cpp
+++ Source/BetterGameplayTags/Private/BGTBlueprintLibrary.cpp
@@ -11,3 +11,25 @@ FGameplayTag UBGTBlueprintLibrary::Conv_NameToGameplayTag(FName Name)
 {
 	return FGameplayTag::RequestGameplayTag(Name, true);
 }
+
+FGameplayTagContainer UBGTBlueprintLibrary::FilterTags(const FGameplayTagContainer &Tags, const FGameplayTagContainer &Filter, bool bExactMatch)
+{
+	return bExactMatch ? Tags.FilterExact(Filter) : Tags.Filter(Filter);
+
+}
+
+TArray<FGameplayTag> UBGTBlueprintLibrary::GetTagsWithParent(const FGameplayTagContainer &Tags, FGameplayTag ParentTag)
+{
+	TArray<FGameplayTag> ReturnArray;
+
+	for (auto TagIt = Tags.CreateConstIterator(); TagIt; ++TagIt )
+	{
+		FGameplayTag Tag = *TagIt;
+		if (Tag.MatchesTag(ParentTag))
+		{
+			ReturnArray.Add(Tag);
+		}
+	}
+
+	return ReturnArray;
+}
--- Source/BetterGameplayTags/Public/BGTBlueprintLibrary.h
+++ Source/BetterGameplayTags/Public/BGTBlueprintLibrary.h
@@ -4,7 +4,7 @@
 #include "GameplayTagContainer.h"
 #include "BGTBlueprintLibrary.generated.h"
 
-UCLASS()
+UCLASS(meta = (ScriptName = "BetterGameplayTagLibrary", DisplayName = "Better Gameplay Tag Library"))
 class UBGTBlueprintLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
@@ -16,5 +16,27 @@ public:
 	UFUNCTION(BlueprintPure, meta = (DisplayName = "Name To Gameplay Tag", CompactNodeTitle = "->", BlueprintAutocast), Category = "Better Gameplay Tags")
 	static FGameplayTag Conv_NameToGameplayTag(FName Name);
 
+	/**
+	 * Returns a filtered version of this container, returns all tags that match against the tags in FilterContainer
+	 *
+	 * @param TagContainer			Container that needs filtering
+	 * @param FilterContainer		The container to filter against
+	 * @param ExactMatch			If true, tags must exactly match one in FilterContainer. If false, tags can match by expanding parents.
+	 *
+	 * @return A FGameplayTagContainer containing the filtered tags
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Better Gameplay Tags")
+	static FGameplayTagContainer FilterTags(const FGameplayTagContainer &TagContainer, const FGameplayTagContainer &FilterContainer, bool bExactMatch = false);
+
+	/**
+	 * Returns all tags in TagContainer that match the ParentFilter tag, expanding parents.
+	 *
+	 * @param TagContainer			Container To Search
+	 * @param ParentTag				The Parent Tag To expand
+	 *
+	 * @return An array of FGameplayTags containing the children of ParentTags from TagContainer
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Better Gameplay Tags")
+	static TArray<FGameplayTag> GetTagsWithParent(const FGameplayTagContainer &TagContainer, FGameplayTag ParentTag);
 
 };
